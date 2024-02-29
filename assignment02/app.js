290aeff674bbf8e69434151d7d2081e2464f3e68
Noman pervaiz <153602037+Nomanpervaiz@users.noreply.github.com>
2024-02-29 16:37:10 +0500

    Add files via upload

---
+++ assignment02/app.js
@@ -0,0 +1,205 @@
+
+// Chapter 04 (Legal & Illegal)
+
+// Solved Q1
+// var fullName=("Noman pervaiz");
+// var field=("Information Technology");
+// var course=("Web and app development");
+// console.log("My name is "+fullName+" My field name "+field+" currently studing "+course);
+
+
+// Solved Q2
+// illegal varibles
+// var 1stnum=("varible is illegal");
+// var 1numOne=("varible is illegal");
+// var num-one=("varible is illegal");
+// var 2_num=("varible is illegal");
+// var var=("varible is illegal");
+// Legal varibles
+// var num1=("varible one is legal");
+// var num1Name1=("varible two is legal");
+// var numOne=("varible three is legal");
+// var camelCaseIsLegal=("varible four is legal");
+// var num_1=("varible five is legal");
+// console.log(num1);
+// console.log(num1Name1);
+// console.log(numOne);
+// console.log(camelCaseIsLegal);
+// console.log(num_1);
+
+
+// Solved Q3
+// var heading=("<h1>Rules for naming JS varibles</h1>");
+// document.write(heading);
+// document.write("Varible names can only contain numbers,$ and_. For example: $my_1stVarible. <br>");
+// document.write("Varible must begin with a letter, $ or_. For example: $fullName, _fullName or fullName. <br>");
+// document.write("Varible names are case sensitive. <br>");
+// document.write("Varible names should not be JS keywords.");
+
+
+//chapter 05 (Math Expression)
+
+// Solved Q1
+// var num1=3;
+// var num2=5;
+// document.write("Sum of "+num1+" and "+num2+" is "+(num1+num2));
+
+
+// Solved Q2
+// var num1=10;
+// var num2=5;
+// document.write("Sub of "+num1+" and "+num2+" is "+(num1-num2)+"<br>");
+// var num1=10;
+// var num2=5;
+// document.write("Multiplication of "+num1+" and "+num2+" is "+(num1*num2)+"<br>" );
+// var num1=10;
+// var num2=5;
+// document.write("Division of "+num1+" and "+num2+" is "+(num1/num2)+"<br>");
+// var num1=10;
+// var num2=5;
+// document.write("Reminder of "+num1+" and "+num2+" is "+(num1%num2));
+
+
+// Solved Q3
+// var value;
+// document.write("Value after variable declaration is "+value+"<br>")
+// var value=5;
+// document.write("Initial value "+value +"<br>")
+// var value=5;
+// document.write("value aftre increament is: "+(++value)+"<br>")
+// var value=5;
+// document.write("value aftre Addition is: "+(++value+7)+"<br>")
+// var value=5;
+// document.write("value aftre decreament is: "+(value++ +7)+"<br>")
+// var value=5;
+// document.write("The reminder is: "+value%1)
+
+
+// Solved Q4
+// var cost=600;
+// var ticket=5;
+// document.write("Total cost to buy "+ticket+" tickets to a movie is "+(cost*ticket)+"PKR");
+
+
+// Solved Q5
+// var num=5;  
+// document.write("<h3>Table of 5:</h3> <br>");
+// document.write("5 x 1 = "+num* 1+"<br>");
+// document.write("5 x 2 = "+num* 2+"<br>");
+// document.write("5 x 3 = "+num* 3+"<br>");
+// document.write("5 x 4 = "+num* 4+"<br>");h
+// document.write("5 x 5 = "+num* 5+"<br>");
+// document.write("5 x 6 = "+num* 6+"<br>");
+// document.write("5 x 7 = "+num* 7+"<br>");
+// document.write("5 x 8 = "+num* 8+"<br>");
+// document.write("5 x 9 = "+num* 9+"<br>");
+// document.write("5 x 10 = "+num* 10+"<br>");
+
+
+//  Solced Q6
+// skipping....
+
+
+// Solved Q7
+// var item1=650;
+// var item2=100;
+// var quantity1=3;
+// var quantity2=7;
+// var charges=100;
+// document.write("<h1>Shopping cart</h1>");
+// document.write("Total cost of your order is "+(item1*quantity1+item2*quantity2+charges) )
+
+
+// Solved Q8
+// var totalMarks=980;
+// var obtainMarks=804;
+// document.write("<h1>Mark-sheet:</h1>");
+// document.write("Total marks : "+totalMarks+"<br>")
+// document.write("marks obtained : "+obtainMarks +"<br>")
+// document.write( "Percentage:" +(obtainMarks*100/totalMarks) )
+
+
+// Solved Q9
+// var dollar= 104.80;
+// var riyal= 25;
+// var totalDollar=10;
+// var totalRiyal=28;
+// document.write("<h1>Currency in PKR: </h1>");
+// document.write("Total currency in PKR : "+(dollar*totalDollar+riyal*totalRiyal));
+
+
+// Solved Q10
+// var num0=20
+// var num1=5;
+// var num2=10;
+// var num3=2;
+// document.write("Here our value === > "+(num0+num1*num2/num3));
+
+
+// Solved Q11
+// var currentYear=2024;
+// var birthYear=2002;
+// document.write("<h1>Age Calculator </h1>");
+// document.write("Current Year :" +" "+currentYear+"<br>");
+// document.write("Birth Year :" +" "+birthYear +"<br>");
+// document.write("Your age is :" +" "+currentYear%birthYear +"<br>");
+
+
+// Solved Q12
+// var radius=20;
+// var circumference=3.142
+// document.write("<h1>The Geometrizer</h1> "+"<br>");
+// document.write("Radius of a circle : "+radius +"<br>");
+// document.write("The circumference is : "+radius*2*circumference+"<br>");
+// document.write("The area is : "+radius*20*circumference+"<br>");
+
+
+// Solved Q13
+// var snack=("Chocolate super");
+// var age =22;
+// var maxAge=60;
+// var amountOfSnack=2;
+// document.write("<h1>The Lifetime Supply Calculator</h1> "+"<br>");
+// document.write("Favourite Snack : "+snack+"<br>");
+// document.write("Current age : "+age+"<br>");
+// document.write("Estimated maximum age  : "+maxAge+"<br>");
+// document.write("Amount of snacks per day : "+amountOfSnack+"<br>");
+// document.write("You will need "+(amountOfSnack*maxAge-age)+" "+snack+"to last you until the rip old age of "+maxAge);
+
+
+// Chapter 6 to 7 
+
+// Solved Q1
+// var a=10;
+// document.write("Results: <br>")
+// document.write("The value of a is : "+(a)+"<br>");
+// document.write("................................")
+// document.write("<br>")
+// document.write("<br>")
+// document.write("The value of  ++a is : "+(++a)+"<br>");
+// document.write("Now the value of a is : " +(a))
+// document.write("<br>")
+// document.write("<br>")
+// document.write("The value of  a++ is : "+(a++)+"<br>");
+// document.write("Now the value of a is : " +(a))
+// document.write("<br>")
+// document.write("<br>")
+// document.write("The value of  a-- is : "+(--a)+"<br>");
+// document.write("Now the value of a is : " +(a))
+// document.write("<br>")
+// document.write("<br>")
+// document.write("The value of  a-- is : "+(a)+"<br>");
+// document.write("Now the value of a is : " +(--a))
+
+
+// Solved Q2
+// var a = 2;
+// var b = 1;
+// --a;
+// --a - --b;
+// --a - --b + ++a;
+// --a - --b + ++b + b--;
+// document.write("a is"+" "+ a +"<br>", "b is"+" "+ b +"<br>");
+// document.write("Result is"+" "+(a+b));
+
+// Chapter 4,5,6,7 Exercise End here !
